0bfee28b6d528c27a391b870dbf6e86c14a72081
Vladimir Jovanovic <v.jovanovic@holidaypirates.com>
2017-01-25 17:49:29 +0100

    New icon Link to github from main activity

--- sample/src/main/java/com/vlad1m1r/lemniscate/sample/MainActivity.java
+++ sample/src/main/java/com/vlad1m1r/lemniscate/sample/MainActivity.java
@@ -1,11 +1,16 @@
 package com.vlad1m1r.lemniscate.sample;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.vlad1m1r.lemniscate.base.BaseCurveProgressView;
 
@@ -19,13 +24,16 @@ public class MainActivity extends AppCompatActivity implements FragmentCurve.OnV
     private ViewPager mPager;
 
     private CurvesPagerAdapter mPagerAdapter;
-
+    private Toolbar mToolbar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
 
         mFragmentSettings = (FragmentSettings) getSupportFragmentManager().findFragmentById(R.id.fragment_settings);
         mPager = (ViewPager) findViewById(viewPager);
@@ -59,5 +67,24 @@ public class MainActivity extends AppCompatActivity implements FragmentCurve.OnV
         public int getCount() {
             return NUM_PAGES;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_github:
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/vlad1m1r990/Lemniscate"));
+                startActivity(browserIntent);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
